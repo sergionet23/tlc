56de18f43eefc2f92d4a072da5b752bb5a2c617e
Sergio <sergionet23@gmail.com>
2020-03-03 14:27:14 -0300

    master

---
+++ TeLoCuido/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TelocuidoConfig(AppConfig):
+    name = 'TeLoCuido'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sergi\Documents\Particular\01 - Programa BIT\Proyecto 1er año\tlc\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
