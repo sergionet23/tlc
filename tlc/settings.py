9c3a426fe6ccf73fb1f844806acd24fd7dd93c52
Sergio <sergionet23@gmail.com>
2020-03-03 17:14:38 -0300

    3er commit

--- tlc/settings.py
+++ tlc/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist.../
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist....../
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(*kvfvtd3=^w*m3etnsj*&1mpy-_nyvig^c29oczexc7^ppmxn'
